0b7cfcf13d7c686a3dfe580297c4565d5ce8ef2d
Vinicius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2017-12-02 14:47:25 -0200

    UPDATE

--- view/e2e/app.e2e-spec.ts
+++ view/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('view App', () => {
+describe('angular4 App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- view/src/app/app.module.ts
+++ view/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductsComponent } from './products/products.component';
+import { AllanComponent } from './allan/allan.component';
 
+const appRoutes: Routes = [
+  { path : '', pathMatch: 'full', component : ProductsComponent },
+  { path : 'products', component : ProductsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductsComponent,
+    AllanComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
